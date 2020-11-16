1e35d9065d19f47276f7615dbb5c362a324c3374
naresh <agrawal.nar@northeastern.edu>
2020-11-16 15:33:46 -0500

    a7

--- src/main/java/com/webapp/backend/controller/UserController.java
+++ src/main/java/com/webapp/backend/controller/UserController.java
@@ -7,6 +7,10 @@ import com.webapp.backend.response.Errors;
 import com.webapp.backend.response.Message;
 import com.webapp.backend.service.UserService;
 import com.webapp.backend.validator.UserValidator;
+import io.prometheus.client.Counter;
+import io.prometheus.client.Summary;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -35,10 +39,14 @@ public class UserController {
     @Autowired
     Authentication auth;
 
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    static final Counter userCount = Counter.build().name("user_operations_count").labelNames("request").help("Total request to user endpoint .").register();
+    static final Summary userMetric = Summary.build().name("user_operations_time").labelNames("request","operation").help("All user operations time.").register();
 
     @RequestMapping(value="/register", method = RequestMethod.POST,  produces = "application/json")
     public ResponseEntity<Object> createUser( @RequestBody User user, BindingResult result) {
-
+        try {
             validator.validate(user, result);
 
             if (result.hasErrors()) {
@@ -48,24 +56,38 @@ public class UserController {
                             @Override
                             public void accept(FieldError action) {
                                 errors.add(new Message(action.getDefaultMessage()));
+                                log.warn(String.valueOf(action.getDefaultMessage()));
                             }
                         });
                 return new ResponseEntity<>(new Errors(errors), HttpStatus.BAD_REQUEST);
-            }else{
-                    user.setAccountCreated(new Date());
-                    user.setAccountUpdated(new Date());
-                    userService.saveUser(user);
-                    return new ResponseEntity<>(user,HttpStatus.CREATED);
+            } else {
+                userCount.labels("GET/register").inc();
+                Summary.Timer requestTimer = userMetric.labels("GET/register","create").startTimer();
+                user.setAccountCreated(new Date());
+                user.setAccountUpdated(new Date());
+                userService.saveUser(user);
+                requestTimer.observeDuration();
+                log.info("User created, UserID:" + user.getUserID());
+                return new ResponseEntity<>(user, HttpStatus.CREATED);
             }
+        }
+        catch (Exception e){
+            log.error(e.getMessage());
+            return new ResponseEntity<>(new Message(e.getMessage()), HttpStatus.BAD_REQUEST);
+        }
     }
 
     @RequestMapping(value="/{id}", method = RequestMethod.GET,  produces = "application/json")
     public ResponseEntity<Object> getUserBYID(@PathVariable("id") Long id){
             User user =userService.userBYID(id);
-            if(user!= null)
-                return new ResponseEntity<>(user,HttpStatus.OK);
-            else
-                return new ResponseEntity<>(new Message("User not found"),HttpStatus.NOT_FOUND);
+            if(user!= null) {
+                log.info("get user, UserID:"+user.getUserID());
+                return new ResponseEntity<>(user, HttpStatus.OK);
+            } else {
+                userCount.labels("GET/{id}").inc();
+                log.info("User not found, UserID:"+id);
+                return new ResponseEntity<>(new Message("User not found"), HttpStatus.NOT_FOUND);
+            }
     }
 
     @RequestMapping(value="/self", method = RequestMethod.GET,  produces = "application/json")
@@ -74,11 +96,15 @@ public class UserController {
             User user = null;
             try {
                 user = auth.authenticate(headers);
+                userCount.labels("GET/self").inc();
+                log.info("Get/self UserID:"+user.getUserID());
                 return new ResponseEntity<>(user,HttpStatus.OK);
 
             } catch (AuthorizationException e) {
+                log.error(e.getMessage());
                 return new ResponseEntity<>(new Message(e.getMessage()),HttpStatus.UNAUTHORIZED);
             }catch (Exception e){
+                log.error(e.getMessage());
                 return new ResponseEntity<>(new Message(e.getMessage()), HttpStatus.BAD_REQUEST);
             }
 
@@ -90,20 +116,31 @@ public class UserController {
         User u = null;
             try {
                  u = auth.authenticate(headers);
-                if(user.getFirstName().isEmpty() || user.getFirstName()==null )
+                if(user.getFirstName().isEmpty() || user.getFirstName()==null ){
+                    log.warn("Put/self Please enter first name, UserID:"+user.getUserID());
                     return new ResponseEntity<>(new Message("Please enter first name"), HttpStatus.BAD_REQUEST);
-                if(user.getLastName().isEmpty() || user.getLastName()==null )
+                }
+                if(user.getLastName().isEmpty() || user.getLastName()==null ) {
+                    log.warn("Put/self Please enter last name, UserID:"+user.getUserID());
                     return new ResponseEntity<>(new Message("Please enter last name"), HttpStatus.BAD_REQUEST);
+                }
                 if (user.getPassword().isEmpty() || user.getPassword()==null || !validatePassword(user.getPassword())) {
+                    log.warn("Put/self Use Strong Password, UserID:"+user.getUserID());
                     return new ResponseEntity<>(new Message("Use Strong Password"), HttpStatus.BAD_REQUEST);
                 }else{
+                    Summary.Timer requestTimer = userMetric.labels("PUT/self","update").startTimer();
                     user.setAccountUpdated(new Date());
                     User newUser = userService.updateUser(user,u.getEmail());
+                    requestTimer.observeDuration();
+                    userCount.labels("PUT/self").inc();
+                    log.info("User updated, UserID:"+user.getUserID());
                     return new ResponseEntity<>(newUser, HttpStatus.ACCEPTED);
                 }
             }catch (AuthorizationException e) {
+                log.error(e.getMessage());
                 return new ResponseEntity<>(new Message(e.getMessage()),HttpStatus.UNAUTHORIZED);
             }catch (Exception e){
+                log.error(e.getMessage());
                 return new ResponseEntity<>(new Message(e.getMessage()), HttpStatus.BAD_REQUEST);
             }
 
--- src/main/java/com/webapp/backend/controller/WatchController.java
+++ src/main/java/com/webapp/backend/controller/WatchController.java
@@ -9,9 +9,11 @@ import com.webapp.backend.response.Message;
 import com.webapp.backend.service.AlertService;
 import com.webapp.backend.service.WatchService;
 
-import org.apache.kafka.common.internals.Topic;
+import io.prometheus.client.Counter;
+import io.prometheus.client.Summary;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -45,7 +47,12 @@ public class WatchController {
 
     private final String TOPIC= "watch";
 
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
+    static final Counter publishCount = Counter.build().name("message_published_total_watch").labelNames("status","topic").help("Total message published on watch topic.").register();
+    static final Counter watchCount = Counter.build().name("watch_operations_count").labelNames("request").help("Total request to watch endpoint .").register();
+    static final Summary watchMetric = Summary.build().name("watch_operation").labelNames("request","operation").help("All watch operations time.").register();
+    static final Summary publishTime = Summary.build().name("publish_time_watch").labelNames("status","topic").help("Publish time on watch topic.").register();
 
     @RequestMapping(value="/", method = RequestMethod.POST,  produces = "application/json")
     public ResponseEntity<Object> createWatch(@RequestBody Watch watch, @RequestHeader HttpHeaders headers ) {
@@ -54,14 +61,25 @@ public class WatchController {
         try {
             user = auth.authenticate(headers);
 
+            Summary.Timer requestTimer = watchMetric.labels("POST/","create").startTimer();
             watch.setUserID(user);
             watchService.save(watch);
+            requestTimer.observeDuration();
+            log.info("Watch created, WatchID:"+ watch.getWatchID());
             //publish watch on kafka
+            publishCount.labels("create","watch").inc();
+
+            Summary.Timer kafkaTimer = publishTime.labels("create","watch").startTimer();
             kafkaTemplate.send(TOPIC, jsonConverter.toJson(watch));
+            kafkaTimer.observeDuration();
+            watchCount.labels("POST/").inc();
+            log.info("Watch published(created), Topic: watch, WatchID:"+watch.getWatchID());
             return new ResponseEntity<>(watch, HttpStatus.OK);
         } catch (AuthorizationException e) {
+            log.error(e.getMessage());
             return new ResponseEntity<>(new Message(e.getMessage()), HttpStatus.UNAUTHORIZED);
         }catch (Exception e){
+            log.error(e.getMessage());
             return new ResponseEntity<>(new Message(e.getMessage()), HttpStatus.BAD_REQUEST);
         }
     }
@@ -75,14 +93,21 @@ public class WatchController {
             Boolean flag = watchService.checkUserWatch(watchID,user.getUserID());
 
             if( flag ) {
+                Summary.Timer requestTimer = watchMetric.labels("GET/{watchID}","get_single_watch").startTimer();
                 Watch watch  =watchService.getWatch(watchID);
+                requestTimer.observeDuration();
+                log.info("get watch, WatchID:"+watchID);
+                watchCount.labels("GET/{watchID}").inc();
                 return new ResponseEntity<>(watch, HttpStatus.OK);
+            } else {
+                log.info("Watch not found, WatchID:"+watchID);
+                return new ResponseEntity<>(new Message("Watch not found"), HttpStatus.NOT_FOUND);
             }
-            else
-                return new ResponseEntity<>(new Message("Watch not found"),HttpStatus.NOT_FOUND);
         }catch (AuthorizationException e) {
+            log.error(e.getMessage());
             return new ResponseEntity<>(new Message(e.getMessage()), HttpStatus.UNAUTHORIZED);
         }catch (Exception e){
+            log.error(e.getMessage());
             return new ResponseEntity<>(new Message(e.getMessage()), HttpStatus.BAD_REQUEST);
         }
     }
@@ -96,17 +121,29 @@ public class WatchController {
             Boolean flag = watchService.checkUserWatch(watchID,user.getUserID());
 
             if (flag) {
+                Summary.Timer requestTimer = watchMetric.labels("PUT/{watchID}","update").startTimer();
                 Watch watch = watchService.getWatch(watchID);
                 watch = watchService.updateWatch(watch, updateWatch);
+                requestTimer.observeDuration();
+                log.info("Watch updated, WatchID:"+ watchID);
                 //publish watch on kafka
+                publishCount.labels("update","watch").inc();
+                watchCount.labels("PUT/{watchID}").inc();
+                Summary.Timer kafkaTimer = publishTime.labels("update","watch").startTimer();
                 kafkaTemplate.send(TOPIC, jsonConverter.toJson(watch));
+                kafkaTimer.observeDuration();
+
+                log.info("Watch published(updated), Topic: watch, WatchID:"+watch.getWatchID());
                 return new ResponseEntity<>(watch, HttpStatus.ACCEPTED);
-            }
-            else
+            } else {
+                log.info("Watch not found, WatchID:"+watchID);
                 return new ResponseEntity<>(new Message("Watch not found"), HttpStatus.NOT_FOUND);
+            }
         } catch (AuthorizationException e) {
+            log.error(e.getMessage());
             return new ResponseEntity<>(new Message(e.getMessage()), HttpStatus.UNAUTHORIZED);
         }catch (Exception e){
+            log.error(e.getMessage());
             return new ResponseEntity<>(new Message(e.getMessage()), HttpStatus.BAD_REQUEST);
         }
     }
@@ -120,17 +157,31 @@ public class WatchController {
             Boolean flag = watchService.checkUserWatch(watchID,user.getUserID());
 
             if (flag) {
+                Summary.Timer requestTimer = watchMetric.labels("DELETE/{watchID}","delete").startTimer();
                 Watch watch = watchService.getWatch(watchID);
                 watch.setStatus("Deleted");
                 //publish watch on kafka
+                log.info("Watch published(deleted), Topic: watch, WatchID:"+watch.getWatchID());
+
+                Summary.Timer kafkaTimer = publishTime.labels("delete","watch").startTimer();
                 kafkaTemplate.send(TOPIC, jsonConverter.toJson(watch));
+                kafkaTimer.observeDuration();
+
                 watchService.deleteWatch(watchID);
+                requestTimer.observeDuration();
+                publishCount.labels("delete","watch").inc();
+                log.info("Watch deleted, WatchID:"+watchID);
+                watchCount.labels("DELETE/{watchID}").inc();
                 return new ResponseEntity<>( HttpStatus.NO_CONTENT);
-            } else
+            } else {
+                log.info("Watch not found, WatchID"+watchID);
                 return new ResponseEntity<>(new Message("Watch not found"), HttpStatus.NOT_FOUND);
+            }
         } catch (AuthorizationException e) {
+            log.error(e.getMessage());
             return new ResponseEntity<>(new Message(e.getMessage()), HttpStatus.UNAUTHORIZED);
         } catch (Exception e) {
+            log.error(e.getMessage());
             return new ResponseEntity<>(new Message(e.getMessage()), HttpStatus.BAD_REQUEST);
         }
     }
@@ -141,15 +192,22 @@ public class WatchController {
 
         try {
             user = auth.authenticate(headers);
-
+            Summary.Timer requestTimer = watchMetric.labels("GET/all","get_all_watch").startTimer();
             LinkedHashSet<Watch> watch = watchService.getUserWatches(user.getUserID());
+            requestTimer.observeDuration();
             if (watch != null) {
+                log.info("get all watch of userID:"+user.getUserID());
+                watchCount.labels("GET/all").inc();
                 return new ResponseEntity<>(watch, HttpStatus.OK);
-            } else
+            } else {
+                log.info("No watch available of userID:"+user.getUserID());
                 return new ResponseEntity<>(new Message("No watch available"), HttpStatus.NO_CONTENT);
+            }
         } catch (AuthorizationException e) {
+            log.error(e.getMessage());
             return new ResponseEntity<>(new Message(e.getMessage()), HttpStatus.UNAUTHORIZED);
         } catch (Exception e) {
+            log.error(e.getMessage());
             return new ResponseEntity<>(new Message(e.getMessage()), HttpStatus.BAD_REQUEST);
         }
     }
